4a8a8ca582f04a27d61a9bd9535d926f40b4e271
Justy116 <giustinogragnaniello116@gmail.com>
2022-10-20 17:46:02 +0200

    first commit

---
+++ src/components/About.js
@@ -0,0 +1,325 @@
+import { useContext } from "react";
+import NavContext from "../context/navContext";
+const About = () => {
+  const { nav } = useContext(NavContext);
+  return (
+    <section id="about" className={`${nav === "about" ? "active" : ""}`}>
+      {/* Main Heading Starts */}
+      <div className="container page-title text-center">
+        <h2 className="text-center">
+          about <span>me</span>
+        </h2>
+        <span className="title-head-subtitle">
+          I design and code beautiful things, and I love what I do.
+        </span>
+      </div>
+      {/* Main Heading Ends */}
+      <div className="container infos">
+        <div className="row personal-info">
+          {/* Personal Infos Starts */}
+          <div className="col-xl-6 col-lg-6 col-md-12 col-sm-12">
+            <div className="image-container">
+              <img className="img-fluid d-block" src="img/me.jpg" alt="" />
+            </div>
+            <p className="d-block d-md-none">
+              {`I'm`} a Freelance UI/UX Designer and Developer based in London,
+              England. I strives to build immersive and beautiful web
+              applications through carefully crafted code and user-centric
+              design.
+            </p>
+          </div>
+          <div className="row col-xl-6 col-lg-6 col-md-12">
+            <div className="col-xl-6 col-lg-6 col-md-6 col-sm-6">
+              <ul className="list-1">
+                <li>
+                  <h6>
+                    <span className="font-weight-600">First Name</span>
+                    Giustino
+                  </h6>
+                </li>
+                <li>
+                  <h6>
+                    <span className="font-weight-600">Last Name</span>
+                    Gragnaniello
+                  </h6>
+                </li>
+                <li>
+                  <h6>
+                    <span className="font-weight-600">Birthdate</span>15
+                    February 2003
+                  </h6>
+                </li>
+                <li>
+                  <h6>
+                    <span className="font-weight-600">Nationality</span>
+                    Italy
+                  </h6>
+                </li>
+              </ul>
+            </div>
+            <div className="col-xl-6 col-lg-6 col-md-6 col-sm-6">
+              <ul className="list-2">
+                <li>
+                  <h6>
+                    <span className="font-weight-600">Langages</span>
+                    Italian - English
+                  </h6>
+                </li>
+                <li>
+                  <h6>
+                    <span className="font-weight-600">Email</span>
+                    giustinogragnaniello16@gmail.com
+                  </h6>
+                </li>
+                <li>
+                  <h6>
+                    <span className="font-weight-600">Instagram</span>
+                    gragnaniello_giustino
+                  </h6>
+                </li>
+              </ul>
+            </div>
+            <div className="col-12 resume-btn-container">
+              <a href="#" className="btn btn-resume">
+                <span>
+                  <i className="fa fa-download" />
+                  download my cv
+                </span>
+              </a>
+            </div>
+          </div>
+          {/* Personal Infos Ends */}
+        </div>
+      </div>
+      {/* Download CV Starts */}
+      <div className="container col-12 mx-auto text-center">
+        <hr className="about-section" />
+      </div>
+      {/* Download CV Ends */}
+      {/* Resume Starts */}
+      <div className="resume-container">
+        <div className="container">
+          <div className="row">
+            {/* Experience Starts */}
+            <div className="col-xl-6 col-lg-6 col-md-6">
+              <h2 className="font-weight-600 uppercase title-section">
+                experience
+              </h2>
+              <div className="resume-items">
+                {/* Item Starts */}
+                <div className="item">
+                  <span className="bullet" />
+                  <div className="card">
+                    <div className="card-header">
+                      <span className="year">
+                        <i className="fa fa-calendar" />
+                        <i className="fa fa-caret-right" />
+                        2022 - &infin;
+                      </span>
+                      <span className="d-block font-weight-400 uppercase">
+                        web developer
+                        <span className="separator" />
+                        <span className="font-weight-700">Accenture</span>
+                      </span>
+                    </div>
+                    <div className="card-body">
+                      <p>
+                        Ho iniziato la mia avventura nel mondo del lavoro qui e
+                        spero che continui per piu tempo possibile.
+                      </p>
+                    </div>
+                  </div>
+                </div>
+                {/* Item Ends */}
+              </div>
+            </div>
+            {/* Experience Ends */}
+            {/* Education Starts */}
+            <div className="col-xl-6 col-lg-6 col-md-6 skills-container">
+              <h2 className="font-weight-600 uppercase title-section">
+                Education
+              </h2>
+              <div className="resume-items">
+                {/* Item Starts */}
+                <div className="item">
+                  <span className="bullet" />
+                  <div className="card">
+                    <div className="card-header">
+                      <span className="year">
+                        <i className="fa fa-calendar" />
+                        <i className="fa fa-caret-right" />
+                        2021 - 2021
+                      </span>
+                      <span className="d-block font-weight-400 uppercase">
+                        Web developer
+                        <span className="separator" />
+                        <span className="font-weight-700">Start2Impact</span>
+                      </span>
+                    </div>
+                    <div className="card-body">
+                      <p>
+                        Il mio primo approccio nel mondo della programmazione
+                        web è stato un corso su start2Impact (modificare).
+                      </p>
+                    </div>
+                  </div>
+                </div>
+                {/* Item Ends */}
+                {/* Item Starts */}
+                <div className="item">
+                  <span className="bullet" />
+                  <div className="card">
+                    <div className="card-header">
+                      <span className="year">
+                        <i className="fa fa-calendar" />
+                        <i className="fa fa-caret-right" />
+                        2021 - 2021
+                      </span>
+                      <span className="d-block font-weight-400 uppercase">
+                        Workfully
+                        <span className="separator" />
+                        <span className="font-weight-700">Web developer</span>
+                      </span>
+                    </div>
+                    <div className="card-body">
+                      <p>
+                        Dopo esperienza terribile in start2impoact ho avuto
+                        occasione di poter fare questo corso per loro e devo
+                        dire mi sono trovato bene (modificare)
+                      </p>
+                    </div>
+                  </div>
+                </div>
+                {/* Item Ends */}
+              </div>
+            </div>
+            {/* Education Ends */}
+          </div>
+          {/* Skills Starts */}
+          <div className="row">
+            {/* Skill Bar Starts */}
+            <div className="col-12">
+              <h2 className="font-weight-600 uppercase title-section skills-title">
+                skills
+              </h2>
+            </div>
+            {/* Skill Bar Starts */}
+            <div className="col-12 col-sm-6 col-md-4">
+              <span className="skill-text">html</span>
+              <div className="chart-bar">
+                <span
+                  className="item-progress"
+                  data-percent={90}
+                  style={{ width: "90%" }}
+                />
+                <span className="percent" style={{ right: "calc(20% - 21px)" }}>
+                  90%
+                  <b className="arrow" />
+                </span>
+              </div>
+            </div>
+            {/* Skill Bar Ends */}
+            {/* Skill Bar Starts */}
+            <div className="col-12 col-sm-6 col-md-4">
+              <span className="skill-text">javascript</span>
+              <div className="chart-bar">
+                <span
+                  className="item-progress"
+                  data-percent={70}
+                  style={{ width: "70%" }}
+                />
+                <span className="percent" style={{ right: "calc(40% - 21px)" }}>
+                  70%
+                  <b className="arrow" />
+                </span>
+              </div>
+            </div>
+            {/* Skill Bar Ends */}
+            {/* Skill Bar Starts */}
+            <div className="col-12 col-sm-6 col-md-4">
+              <span className="skill-text">css</span>
+              <div className="chart-bar">
+                <span
+                  className="item-progress"
+                  data-percent={90}
+                  style={{ width: "90%" }}
+                />
+                <span className="percent" style={{ right: "calc(25% - 21px)" }}>
+                  90%
+                  <b className="arrow" />
+                </span>
+              </div>
+            </div>
+            {/* Skill Bar Starts */}
+            <div className="col-12 col-sm-6 col-md-4">
+              <span className="skill-text">wordpress</span>
+              <div className="chart-bar">
+                <span
+                  className="item-progress"
+                  data-percent={80}
+                  style={{ width: "80%" }}
+                />
+                <span className="percent" style={{ right: "calc(10% - 21px)" }}>
+                  80%
+                  <b className="arrow" />
+                </span>
+              </div>
+            </div>
+            {/* Skill Bar Ends */}
+            {/* Skill Bar Starts */}
+            <div className="col-12 col-sm-6 col-md-4">
+              <span className="skill-text">angular js</span>
+              <div className="chart-bar">
+                <span
+                  className="item-progress"
+                  data-percent={75}
+                  style={{ width: "75%" }}
+                />
+                <span className="percent" style={{ right: "calc(15% - 21px)" }}>
+                  75%
+                  <b className="arrow" />
+                </span>
+              </div>
+            </div>
+            {/* Skill Bar Ends */}
+            {/* Skill Bar Starts */}
+            <div className="col-12 col-sm-6 col-md-4">
+              <span className="skill-text">React</span>
+              <div className="chart-bar">
+                <span
+                  className="item-progress"
+                  data-percent={80}
+                  style={{ width: "80%" }}
+                />
+                <span className="percent" style={{ right: "calc(40% - 21px)" }}>
+                  80%
+                  <b className="arrow" />
+                </span>
+              </div>
+            </div>
+            {/* Skill Bar Ends */}
+            {/* Skill Bar Starts */}
+            <div className="col-12 col-sm-6 col-md-4">
+              <span className="skill-text">Vue </span>
+              <div className="chart-bar">
+                <span
+                  className="item-progress"
+                  data-percent={75}
+                  style={{ width: "75%" }}
+                />
+                <span className="percent" style={{ right: "calc(25% - 21px)" }}>
+                  75%
+                  <b className="arrow" />
+                </span>
+              </div>
+            </div>
+            {/* Skill Bar Ends */}
+          </div>
+          {/* Skills Starts */}
+        </div>
+        {/* Resume Ends */}
+      </div>
+    </section>
+  );
+};
+export default About;
---
+++ src/components/AnimationText.js
@@ -0,0 +1,13 @@
+import MovingComponent from "react-moving-text";
+const AnimationText = () => {
+  return (
+    <h1 className="ah-headline d-flex">
+      {`I'm`}{" "}
+      <MovingComponent
+        type="typewriter"
+        dataText={["Giustino Gragnaniello", "web developer"]}
+      />
+    </h1>
+  );
+};
+export default AnimationText;
